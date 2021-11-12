82648d1c567cbe23d5f9a0fda4aab7559919b19f
Nikki Mounce <nikkimounce@gmail.com>
2021-11-12 08:19:38 -0800

    added get 1 user route

--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -13,3 +13,23 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//get 1 user with associated posts
+router.get(':/id', async (req, res) => {
+    //find 1 user by its `id` value
+    //include all associated posts
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [{ model: Posts }]
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No User found with this ID' });
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
